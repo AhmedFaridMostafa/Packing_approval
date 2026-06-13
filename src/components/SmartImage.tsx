1800e69bb1699cd071cf8f3a2aefea9c8aed726d
Ahmed Fared <ahmed.super997@gmail.com>
2026-06-14 02:38:25 +0300

    feat(ui): replace CloudImage with CldImage and implement SmartImage component

---
+++ src/components/SmartImage.tsx
@@ -0,0 +1,41 @@
+import Image, { type ImageProps } from "next/image";
+import { isCloudinaryUrl } from "@/lib/utils";
+import CldImage from "./CldImage";
+
+type SmartImageProps = Omit<ImageProps, "src"> & {
+  src: string;
+};
+
+const SmartImage = ({
+  src,
+  alt,
+  width,
+  height,
+  className,
+  ...rest
+}: SmartImageProps) => {
+  if (isCloudinaryUrl(src)) {
+    return (
+      <CldImage
+        src={src}
+        alt={alt}
+        width={width}
+        height={height}
+        className={className}
+        {...rest}
+      />
+    );
+  }
+  return (
+    <Image
+      src={src}
+      alt={alt}
+      width={width}
+      height={height}
+      className={className}
+      {...rest}
+    />
+  );
+};
+
+export default SmartImage;
